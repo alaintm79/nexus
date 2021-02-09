1ed0c71f79bc8b93e28cd1a3a3dfc85f9c38e620
alaintm79 <alaintm@etep.une.cu>
2021-02-09 09:32:42 -0500

    Commit #18

--- src/Entity/Logistica/Contrato/Contrato.php
+++ src/Entity/Logistica/Contrato/Contrato.php
@@ -12,6 +12,7 @@ use App\Entity\Traits\TimeStampableTrait;
 use App\Entity\Logistica\ProveedorCliente;
 use App\Entity\Logistica\Contrato\Vigencia;
 use App\Entity\Logistica\Contrato\Categoria;
+use App\Entity\Traits\RegistroTrait;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Repository\Logistica\Contrato\ContratoRepository;
 
@@ -23,7 +24,7 @@ use App\Repository\Logistica\Contrato\ContratoRepository;
  */
 class Contrato
 {
-    use IdTrait, ObservacionTrait, IsModificableTrait, TimeStampableTrait;
+    use IdTrait, RegistroTrait, ObservacionTrait, IsModificableTrait, TimeStampableTrait;
 
     /**
      * @var string
@@ -69,6 +70,11 @@ class Contrato
     private $proveedorCliente;
 
     /**
+     * @ORM\ManyToOne(targetEntity=Vigencia::class)
+     */
+    private $vigencia;
+
+    /**
      * @var float
      *
      * @ORM\Column(name="valor_cup", type="decimal", precision=10, scale=2, nullable=true)
@@ -83,95 +89,6 @@ class Contrato
      * @Assert\Type(type="numeric", groups={"new","edit"})
      */
     private $valorCuc;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="registro_comite", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"approve"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"approve"})
-     * @Assert\LessThanOrEqual(value="999", groups={"approve"})
-     */
-    private $registroComite;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="registro_acuerdo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"approve"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"approve"})
-     * @Assert\LessThanOrEqual(value="999", groups={"approve"})
-     */
-    private $registroAcuerdo;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="cancelado_consejo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"edit"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="999", groups={"cancel"})
-     */
-    private $canceladoComite;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="cancelado_acuerdo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"edit"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="999", groups={"cancel"})
-     */
-    private $canceladoAcuerdo;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Vigencia::class)
-     */
-    private $vigencia;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_aprobado", type="date", nullable=true)
-     * @Assert\Date(groups={"approve"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"approve"})
-     */
-    private $fechaAprobado;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_cancelado", type="date", nullable=true)
-     * @Assert\Date(groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"cancel"})
-     * @Assert\GreaterThan(propertyPath="fechaAprobado",
-     *     message="Fecha fuera de rango",
-     *     groups={"cancel"}
-     * )
-     */
-    private $fechaCancelado;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_firma", type="date", nullable=true)
-     * @Assert\Date(groups={"firm"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"firm"})
-     * @Assert\GreaterThan(propertyPath="fechaAprobado",
-     *     message="Fecha fuera de rango",
-     *     groups={"firm"}
-     * )
-     */
-    private $fechaFirma;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_vigencia", type="date", nullable=true)
-     * @Assert\Date(groups={"new", "edit"})
-     * @Assert\GreaterThanOrEqual(value="today", message="Fecha fuera de rango", groups={"new", "edit"})
-     */
-    private $fechaVigencia;
 
     /**
      * @var float
@@ -243,126 +160,6 @@ class Contrato
     public function setTipo(?string $tipo): self
     {
         $this->tipo = $tipo === 'proveedor' ? 'p' : 'c';
-
-        return $this;
-    }
-
-    public function getValorCup(): ?string
-    {
-        return $this->valorCup;
-    }
-
-    public function setValorCup(?string $valorCup): self
-    {
-        $this->valorCup = $valorCup;
-
-        return $this;
-    }
-
-    public function getValorCuc(): ?string
-    {
-        return $this->valorCuc;
-    }
-
-    public function setValorCuc(?string $valorCuc): self
-    {
-        $this->valorCuc = $valorCuc;
-
-        return $this;
-    }
-
-    public function getRegistroComite(): ?int
-    {
-        return $this->registroComite;
-    }
-
-    public function setRegistroComite(?int $registroComite): self
-    {
-        $this->registroComite = $registroComite;
-
-        return $this;
-    }
-
-    public function getRegistroAcuerdo(): ?int
-    {
-        return $this->registroAcuerdo;
-    }
-
-    public function setRegistroAcuerdo(?int $registroAcuerdo): self
-    {
-        $this->registroAcuerdo = $registroAcuerdo;
-
-        return $this;
-    }
-
-    public function getCanceladoComite(): ?int
-    {
-        return $this->canceladoComite;
-    }
-
-    public function setCanceladoComite(?int $canceladoComite): self
-    {
-        $this->canceladoComite = $canceladoComite;
-
-        return $this;
-    }
-
-    public function getCanceladoAcuerdo(): ?int
-    {
-        return $this->canceladoAcuerdo;
-    }
-
-    public function setCanceladoAcuerdo(?int $canceladoAcuerdo): self
-    {
-        $this->canceladoAcuerdo = $canceladoAcuerdo;
-
-        return $this;
-    }
-
-    public function getFechaAprobado(): ?\DateTimeInterface
-    {
-        return $this->fechaAprobado;
-    }
-
-    public function setFechaAprobado(?\DateTimeInterface $fechaAprobado): self
-    {
-        $this->fechaAprobado = $fechaAprobado;
-
-        return $this;
-    }
-
-    public function getFechaCancelado(): ?\DateTimeInterface
-    {
-        return $this->fechaCancelado;
-    }
-
-    public function setFechaCancelado(?\DateTimeInterface $fechaCancelado): self
-    {
-        $this->fechaCancelado = $fechaCancelado;
-
-        return $this;
-    }
-
-    public function getFechaFirma(): ?\DateTimeInterface
-    {
-        return $this->fechaFirma;
-    }
-
-    public function setFechaFirma(?\DateTimeInterface $fechaFirma): self
-    {
-        $this->fechaFirma = $fechaFirma;
-
-        return $this;
-    }
-
-    public function getFechaVigencia(): ?\DateTimeInterface
-    {
-        return $this->fechaVigencia;
-    }
-
-    public function setFechaVigencia(?\DateTimeInterface $fechaVigencia): self
-    {
-        $this->fechaVigencia = $fechaVigencia;
 
         return $this;
     }
@@ -475,57 +272,32 @@ class Contrato
         return $this;
     }
 
+    public function getValorCup(): ?string
+    {
+        return $this->valorCup;
+    }
+
+    public function setValorCup(?string $valorCup): self
+    {
+        $this->valorCup = $valorCup;
+
+        return $this;
+    }
+
+    public function getValorCuc(): ?string
+    {
+        return $this->valorCuc;
+    }
+
+    public function setValorCuc(?string $valorCuc): self
+    {
+        $this->valorCuc = $valorCuc;
+
+        return $this;
+    }
+
     public function __toString()
     {
         return (string) $this->numero;
     }
-
-    /**
-     * @Assert\IsTrue(
-     *      message = "Fecha.",
-     *      groups={"edit"}
-     * )
-     */
-    // public function isFechaAprobadoValid()
-    // {
-    //     $estado = $this->getEstado()->getEstado();
-
-    //     if($estado === 'CANCELADO' && $this->getTipo() === 'p'){
-    //         if(is_null($this->getCanceladoAcuerdo())){
-    //             return false;
-    //         }
-    //     }
-    // }
-
-    /**
-     * @Assert\IsTrue(
-     *      message = "Falta el número de acuerdo.",
-     *      groups={"edit"}
-     * )
-     */
-    // public function isCanceladoAcuerdoValid()
-    // {
-    //     $estado = $this->getEstado()->getEstado();
-
-    //     if($estado === 'CANCELADO' && $this->getTipo() === 'p'){
-    //         if(is_null($this->getCanceladoAcuerdo())){
-    //             return false;
-    //         }
-    //     }
-    // }
-
-    /**
-     * @Assert\IsTrue(
-     *      message = "Fecha fuera de rango.",
-     *      groups={"edit"}
-     * )
-     */
-    // public function isFechaCanceladoValid()
-    // {
-    //     if($this->getEstado()->getEstado() === 'CANCELADO'){
-    //         if($this->getFechaAprobado() > $this->getFechaCancelado()){
-    //             return false;
-    //         }
-    //     }
-    // }
 }
--- src/Entity/Logistica/Contrato/Suplemento.php
+++ src/Entity/Logistica/Contrato/Suplemento.php
@@ -8,6 +8,7 @@ use App\Entity\Logistica\Contrato\Vigencia;
 use App\Entity\Traits\IdTrait;
 use App\Entity\Traits\IsModificableTrait;
 use App\Entity\Traits\ObservacionTrait;
+use App\Entity\Traits\RegistroTrait;
 use App\Entity\Traits\TimeStampableTrait;
 use App\Repository\Logistica\Contrato\SuplementoRepository;
 use Doctrine\ORM\Mapping as ORM;
@@ -21,7 +22,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Suplemento
 {
-    use IdTrait, ObservacionTrait, TimeStampableTrait, IsModificableTrait;
+    use IdTrait, RegistroTrait, ObservacionTrait, TimeStampableTrait, IsModificableTrait;
 
     /**
      * @ORM\ManyToOne(targetEntity=Contrato::class)
@@ -50,6 +51,11 @@ class Suplemento
     private $estado;
 
     /**
+     * @ORM\ManyToOne(targetEntity=Vigencia::class)
+     */
+    private $vigencia;
+
+    /**
      * @var float
      *
      * @ORM\Column(name="valor_cup", type="decimal", precision=10, scale=2, nullable=true)
@@ -64,95 +70,6 @@ class Suplemento
      * @Assert\Type(type="numeric", groups={"new","edit"})
      */
     private $valorCuc;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="registro_comite", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"approve"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"approve"})
-     * @Assert\LessThanOrEqual(value="999", groups={"approve"})
-     */
-    private $registroComite;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="registro_acuerdo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"approve"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"approve"})
-     * @Assert\LessThanOrEqual(value="999", groups={"approve"})
-     */
-    private $registroAcuerdo;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="cancelado_consejo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"edit"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="999", groups={"cancel"})
-     */
-    private $canceladoComite;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="cancelado_acuerdo", type="integer", nullable=true)
-     * @Assert\Regex(pattern="/^[0-9]+$/i", match=true, message="Valor no permitido", groups={"edit"})
-     * @Assert\GreaterThanOrEqual(value=0, groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="999", groups={"cancel"})
-     */
-    private $canceladoAcuerdo;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Vigencia::class)
-     */
-    private $vigencia;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_aprobado", type="date", nullable=true)
-     * @Assert\Date(groups={"approve"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"approve"})
-     */
-    private $fechaAprobado;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_cancelado", type="date", nullable=true)
-     * @Assert\Date(groups={"cancel"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"cancel"})
-     * @Assert\GreaterThan(propertyPath="fechaAprobado",
-     *     message="Fecha fuera de rango",
-     *     groups={"cancel"}
-     * )
-     */
-    private $fechaCancelado;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_firma", type="date", nullable=true)
-     * @Assert\Date(groups={"firm"})
-     * @Assert\LessThanOrEqual(value="today", message="Fecha fuera de rango", groups={"firm"})
-     * @Assert\GreaterThan(propertyPath="fechaAprobado",
-     *     message="Fecha fuera de rango",
-     *     groups={"firm"}
-     * )
-     */
-    private $fechaFirma;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="fecha_vigencia", type="date", nullable=true)
-     * @Assert\Date(groups={"new", "edit"})
-     * @Assert\GreaterThanOrEqual(value="today", message="Fecha fuera de rango", groups={"new", "edit"})+
-     */
-    private $fechaVigencia;
 
     public function getObjeto(): ?string
     {
@@ -174,126 +91,6 @@ class Suplemento
     public function setTipo(string $tipo): self
     {
         $this->tipo = $tipo;
-
-        return $this;
-    }
-
-    public function getValorCup(): ?string
-    {
-        return $this->valorCup;
-    }
-
-    public function setValorCup(?string $valorCup): self
-    {
-        $this->valorCup = $valorCup;
-
-        return $this;
-    }
-
-    public function getValorCuc(): ?string
-    {
-        return $this->valorCuc;
-    }
-
-    public function setValorCuc(?string $valorCuc): self
-    {
-        $this->valorCuc = $valorCuc;
-
-        return $this;
-    }
-
-    public function getRegistroComite(): ?int
-    {
-        return $this->registroComite;
-    }
-
-    public function setRegistroComite(?int $registroComite): self
-    {
-        $this->registroComite = $registroComite;
-
-        return $this;
-    }
-
-    public function getRegistroAcuerdo(): ?int
-    {
-        return $this->registroAcuerdo;
-    }
-
-    public function setRegistroAcuerdo(?int $registroAcuerdo): self
-    {
-        $this->registroAcuerdo = $registroAcuerdo;
-
-        return $this;
-    }
-
-    public function getCanceladoComite(): ?int
-    {
-        return $this->canceladoComite;
-    }
-
-    public function setCanceladoComite(?int $canceladoComite): self
-    {
-        $this->canceladoComite = $canceladoComite;
-
-        return $this;
-    }
-
-    public function getCanceladoAcuerdo(): ?int
-    {
-        return $this->canceladoAcuerdo;
-    }
-
-    public function setCanceladoAcuerdo(?int $canceladoAcuerdo): self
-    {
-        $this->canceladoAcuerdo = $canceladoAcuerdo;
-
-        return $this;
-    }
-
-    public function getFechaAprobado(): ?\DateTimeInterface
-    {
-        return $this->fechaAprobado;
-    }
-
-    public function setFechaAprobado(?\DateTimeInterface $fechaAprobado): self
-    {
-        $this->fechaAprobado = $fechaAprobado;
-
-        return $this;
-    }
-
-    public function getFechaCancelado(): ?\DateTimeInterface
-    {
-        return $this->fechaCancelado;
-    }
-
-    public function setFechaCancelado(?\DateTimeInterface $fechaCancelado): self
-    {
-        $this->fechaCancelado = $fechaCancelado;
-
-        return $this;
-    }
-
-    public function getFechaFirma(): ?\DateTimeInterface
-    {
-        return $this->fechaFirma;
-    }
-
-    public function setFechaFirma(?\DateTimeInterface $fechaFirma): self
-    {
-        $this->fechaFirma = $fechaFirma;
-
-        return $this;
-    }
-
-    public function getFechaVigencia(): ?\DateTimeInterface
-    {
-        return $this->fechaVigencia;
-    }
-
-    public function setFechaVigencia(?\DateTimeInterface $fechaVigencia): self
-    {
-        $this->fechaVigencia = $fechaVigencia;
 
         return $this;
     }
@@ -330,6 +127,30 @@ class Suplemento
     public function setVigencia(?Vigencia $vigencia): self
     {
         $this->vigencia = $vigencia;
+
+        return $this;
+    }
+
+    public function getValorCup(): ?string
+    {
+        return $this->valorCup;
+    }
+
+    public function setValorCup(?string $valorCup): self
+    {
+        $this->valorCup = $valorCup;
+
+        return $this;
+    }
+
+    public function getValorCuc(): ?string
+    {
+        return $this->valorCuc;
+    }
+
+    public function setValorCuc(?string $valorCuc): self
+    {
+        $this->valorCuc = $valorCuc;
 
         return $this;
     }
